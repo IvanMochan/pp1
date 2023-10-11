d34a752bb7a83bf961ed092a615abfe8978a9257
s-013-15 <s-013-15@gumiforest.ae.krakow.pl>
2023-10-11 18:13:31 +0200

    My second Python programs from textbook

---
+++ 02-TypesAndVariables/Cw10.py
@@ -0,0 +1,6 @@
+name = 'Jan'
+age = '18'
+age2 = "24"
+height = '181'
+n = f'My name is {name}, I am {age} years old, and my height is {height} cm. In 6 years I will be {age2} years old.  '
+print(n)
---
+++ 02-TypesAndVariables/Cw11.py
@@ -0,0 +1,16 @@
+mother_income = 150
+father_income = 150
+fam = 3
+total_income = mother_income + father_income
+person_income = total_income / fam
+n1 = f"Father's income: {father_income}"
+n2 = f"Mother's income: {mother_income}"
+n3 = f"Number of people in family: {fam}"
+n4 = f"Total income: {total_income}"
+n5 = f"Income per person: {person_income}"
+print (n1)
+print (n2)
+print (n3)
+print (n4)
+print (n5)
+
